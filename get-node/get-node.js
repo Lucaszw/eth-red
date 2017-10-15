8a6ba87d59885073c5410b2047297d67d97f978a
lucaszw <lucas.zeer@gmail.com>
2017-10-14 20:31:41 -0400

    get-node updates

--- get-node/get-node.js
+++ get-node/get-node.js
@@ -3,16 +3,36 @@ var mqtt = require('mqtt')
 
 module.exports = function(RED) {
     function GetNode(config) {
-        const id = `get-node:${Date.now()}:${Math.random()*100}`;
-        console.log("Creating node!", id);
-        const client  = mqtt.connect('mqtt://localhost:8083');
-        client.publish("register-node", id);
+        config.id = `get-node:${Date.now()}:${Math.random()*100}`;
+        const topic = `ethereum/node-updated:${config.id}`;
+
+        console.log("Creating node!", config.id);
+        const client  = mqtt.connect('mqtt://localhost:1883');
 
         RED.nodes.createNode(this,config);
-        node = this;
-        node.on('input', function(msg) {
-          node.send({payload: "test"});
+        var node = this;
+
+        client.on('connect', () => {
+          console.log("Connected....", config.id);
+
+          // Listen for changes in the contract value:
+          client.on('message', (_topic, message) => {
+            console.log("received message", _topic);
+            if (_topic != topic) return;
+
+            node.send({payload: "UPDATED!!"});
+          });
+
+          client.subscribe(topic, undefined, () => {
+            console.log("subscribed to : ", topic);
+            client.publish("ethereum/register-node", JSON.stringify(config));
+          });
+
         });
+
+        // node.on('input', function(msg) {
+        //   node.send({payload: "test"});
+        // });
 
     }
     RED.nodes.registerType("get-node",GetNode);
--- webserver/public/web3-interface.js
+++ webserver/public/web3-interface.js
@@ -1,4 +1,14 @@
 // Connect to MQTT and listen for transaction requests:
+const tryParsingJSON = (str) => {
+  let obj;
+  try  {
+    obj = JSON.parse(str);
+  } catch (e) {
+    obj = str;
+  }
+  return obj;
+}
+
 class Web3MqttInterface {
   constructor() {
     const clientId = `Web3Mqtt:${Date.now()}:${Math.random()*100}`;
@@ -8,11 +18,34 @@ class Web3MqttInterface {
     this.client.connect({onSuccess: this.onConnect.bind(this)});
 
     // Keep track of all getter nodes
-    this.getterNodes = new Array();
+    this.nodes = new Map();
   }
 
   registerNode(payload) {
+    this.nodes.set(payload.id, payload);
+    if (this.nodes.size == 1) {
+      setInterval(this.trackChanges.bind(this), 3000);
+    }
     console.log("Registering Node...", payload);
+  }
+
+  trackChanges() {
+    for (const [id, node] of this.nodes) {
+      console.log("checking node", id, node);
+      var contractInterface = JSON.parse(node.interface);
+      var contract = window.web3.eth.contract(contractInterface);
+      var instance = contract.at(node.address);
+      instance.get((...args) => {
+        console.log("args", args);
+        if (args[1] != node.val) {
+          console.log("Node has been updated!");
+          node.val = args[1];
+          this.nodes.set(node.id, node);
+          const topic = `ethereum/node-updated:${node.id}`;
+          this.client.publish(topic, JSON.stringify(node));
+        }
+      });
+    }
   }
 
   makeTransaction(payload) {
@@ -23,21 +56,22 @@ class Web3MqttInterface {
   }
 
   onConnect() {
+    console.log("Subscribing...");
     this.client.subscribe("ethereum/make-transaction");
-    this.client.subscrive("ethereum/register-node");
-    setTimeout(this.checkForTransaction.bind(this), 1000);
+    this.client.subscribe("ethereum/register-node");
   }
 
   onMessageArrived(msg) {
+    console.log("MSG ARRIVED:::");
+    console.log(msg);
+
     const topic = msg.destinationName;
-    const payload = JSON.parse(msg.payloadString);
+
+    const payload = tryParsingJSON(msg.payloadString);
     if (topic == "ethereum/make-transaction")
       this.makeTransaction(payload);
     if (topic == "ethereum/register-node")
       this.registerNode(payload);
-
-    // Ensure the topic is
-    console.log("Message arrived", topic, payload);
   }
 
   onConnectionLost() {
